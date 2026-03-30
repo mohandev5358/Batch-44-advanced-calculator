950a95388a4b8298b58e52f5aa6001ab6b133301
mohandev5358 <mohanpmp129@gmail.com>
2026-03-30 17:23:02 +0530

    Add files via upload

--- assets/main.js
+++ assets/main.js
@@ -1,45 +1,37 @@
 const display = document.getElementById("display");
 
-// Numbers
 document.querySelectorAll('.num').forEach(btn => {
     btn.addEventListener('click', () => {
         display.value += btn.innerText;
     });
 });
 
-// Operators
 document.querySelectorAll('.op').forEach(btn => {
     btn.addEventListener('click', () => {
         display.value += btn.innerText;
     });
 });
 
-// Clear
 document.getElementById("clear").addEventListener("click", () => {
     display.value = "";
 });
 
-// Backspace
 document.getElementById("back").addEventListener("click", () => {
     display.value = display.value.slice(0, -1);
 });
 
-// Equal
 document.getElementById("equal").addEventListener("click", () => {    
         display.value = eval(display.value);
 });
 
-// Square root
 document.getElementById("sqrt").addEventListener("click", () => {
     display.value = Math.sqrt(display.value);
 });
 
-// Cube root
 document.getElementById("cbrt").addEventListener("click", () => {
     display.value = Math.cbrt(display.value);
 });
 
-// Power
 document.getElementById("power").addEventListener("click", () => {
     display.value += "**";
 });
